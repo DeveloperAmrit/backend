f522e03c5d5a0dc7e08de7d57418c2ac47a7e5c2
DeveloperAmrit <kittyamrit005@gmail.com>
2024-12-14 10:58:06 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 
